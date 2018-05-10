1359d9372824367f4357d1fbbcc908f6a96dc734
Eirini Galati <eirini.galati@gmail.com>
2018-05-11 01:07:00 +0200

    start project

---
+++ BinaryTree/Interfaces/INode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree.Interfaces
+{
+    interface INode<T>: IComparable<T>
+    {
+        INode<T> LeftChildNode { get; set; }
+        INode<T> RightChildNode { get; set; }
+    }
+}
